7a5957fbd7c866690f81dd9b82fd206d73d97981
Abdullah Zeyn Hassan <abdullahzen25@gmail.com>
2024-07-30 13:43:44 +0200

    Archive

--- components/Header.vue
+++ components/Header.vue
@@ -4,7 +4,7 @@ import { ref, onMounted, onBeforeUnmount } from 'vue';
 const links = [
   { name: "Home", to: "/", newTab: false },
   { name: "Prints", to: "https://abdullahzeyn.darkroom.com/", newTab: true },
-  { name: "About", to: "/about", newTab: false },
+  { name: "Archive", to: "/Archive", newTab: false },
 ] as { name: string; to: string; newTab: boolean }[];
 
 onMounted(() => {
